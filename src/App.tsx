6764365bdb0cbe392c150c12fcb1f37fb74ca01c
jabo <arnoldjabo@gmail.com>
2026-04-18 18:18:05 +0200

    add SignUp component to App for user registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { lazy } from "react";
+const SignUp = lazy(() => import("./pages/SignUp"));
 function App() {
   return (
     <>
       <h1>vanlife exercises</h1>
+      <SignUp />
     </>
   );
 }
