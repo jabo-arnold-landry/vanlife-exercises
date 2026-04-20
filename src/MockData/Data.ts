0e62f3e5769c3dadaeb7348ac55a58d1b45e780c
jabo <arnoldjabo@gmail.com>
2026-04-20 11:13:57 +0200

    add product information and images for van listings

--- src/MockData/Data.ts
+++ src/MockData/Data.ts
@@ -1,6 +1,70 @@
+import kacyiVanImage from "../assets/kacyi-van image.png";
+import blueVioletVan from "../assets/blue-violet van.png";
+import redWhiteVan from "../assets/red white van.png";
+import kacyiVan2 from "../assets/kacyivan2.png";
+import whiteVan from "../assets/whitevan.png";
+import greenVan from "../assets/greenvan.png";
+
 export const Users = [
   {
     names: "jabo",
     email: "arnoldjabo@gmail.com",
+  },
+];
+
+export const ProductInfo = [
+  {
+    id: "1",
+    img: kacyiVanImage,
+    Pname: "modest explorer",
+    price: "$60/day",
+    Pdescription:
+      "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!",
+    category: "simple",
+  },
+  {
+    id: "2",
+    img: blueVioletVan,
+    Pname: "beach bum",
+    price: "$80/day",
+    Pdescription:
+      "Beach Bum is a van inspired by surfers and travelers. It was created to be a portable home away from home, but with some cool features in it you won't find in an ordinary camper.",
+    category: "rugged",
+  },
+  {
+    id: "3",
+    img: redWhiteVan,
+    Pname: "Reliable Red",
+    price: "$100/day",
+    Pdescription:
+      "Reliable Red is a van that was made for travelling. The inside is comfortable and cozy, with plenty of space to stretch out in. There's a small kitchen, so you can cook if you need to. You'll feel like home as soon as you step out of it.",
+    category: "luxury",
+  },
+  {
+    id: "4",
+    img: kacyiVan2,
+    Pname: "Dreamfinder",
+    price: "$65/day",
+    Pdescription:
+      "Dreamfinder is the perfect van to travel in and experience. With a ceiling height of 2.1m, you can stand up in this van and there is great head room. The floor is a beautiful glass-reinforced plastic (GRP) which is easy to clean and very hard wearing. A large rear window and large side windows make it really light inside and keep it well ventilated.",
+    category: "simple",
+  },
+  {
+    id: "5",
+    img: whiteVan,
+    Pname: "The Cruiser",
+    price: "$120/day",
+    Pdescription:
+      "The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior and ample storage space, the Cruiser offers a beautiful view wherever you go.",
+    category: "luxury",
+  },
+  {
+    id: "6",
+    img: greenVan,
+    Pname: "Green Wonder",
+    price: "$70/day",
+    Pdescription:
+      "With this van, you can take your travel life to the next level. The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.",
+    category: "rugged",
   },
 ];
