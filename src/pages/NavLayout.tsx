06c61ca6f97e674b4a76d236102540eba59aab9c
jabo <arnoldjabo@gmail.com>
2026-04-21 19:30:55 +0200

    fix: replace Anchor with Link for homepage navigation

--- src/pages/NavLayout.tsx
+++ src/pages/NavLayout.tsx
@@ -1,13 +1,13 @@
 import Footer from "../components/Footer";
 import Anchor from "../components/UI/Anchor";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 function NavLayout() {
   return (
     <>
       <nav className="flex justify-between py-6 px-10 items-center">
         <h1>
           {" "}
-          <Anchor to="/" className="text-3xl font-black">#Vanlife</Anchor>
+          <Link to="/" className="text-3xl font-black">#Vanlife</Link>
         </h1>
         <div className="flex flex-wrap gap-10 text-links-color text-xl">
           <Anchor to="about">about</Anchor>
