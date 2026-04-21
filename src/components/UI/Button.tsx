e6dd632777d8e92ba9f3cc4d75e01f260ca93631
jabo <arnoldjabo@gmail.com>
2026-04-21 19:31:10 +0200

    style: enhance Button component with variant styles and improved props handling

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,9 +1,53 @@
 import { type ButtonHTMLAttributes } from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+const buttonStyles = cva(
+  [
+    "rounded-md",
+    "font-text-font",
+    "text-center",
+    "border-none",
+    "capitalize",
+    "text-white",
+    "px-2",
+    "py-1",
+    "font-bold",
+    "cursor-pointer",
+  ],
+  {
+    variants: {
+      color: {
+        bgBlack: ["bg-black"],
+        bgOrange: ["bg-[#FF8C38]"],
+        filtersButton: ["bg-[#FFEAD0]", "text-links-color", "px-4", "py-1"],
+        clearFilter: ["text-links-color", "bg-none", "underline"],
+        simple: ["bg-[#E17654]"],
+        rugged: ["bg-[#115E59]"],
+        luxury: ["bg-black"],
+      },
+      sizes: {
+        sm: ["px-3", "py-1"],
+        md: ["px-5", "py-2"],
+        lg: ["w-[70vh]", "py-3"],
+      },
+    },
+  },
+);
 
-function Button({ children, ...props }: ButtonProps) {
-  return <button {...props}>{children}</button>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonStyles>;
+
+function Button({ children, className, color, sizes, ...props }: ButtonProps) {
+  return (
+    <button
+      className={clsx(twMerge(buttonStyles({ color, sizes }), className))}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default Button;
